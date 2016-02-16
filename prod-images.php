99ef06ce59dc2d9aec934d4e8199ec3799281ce2
Nicolas Kulka <nkulka@beapi.fr>
2016-02-16 14:39:58 +0100

    reformat code with class

--- prod-images.php
+++ prod-images.php
@@ -3,7 +3,7 @@
  * Plugin Name: BEA - Prod images
  * Version: 0.1
  * Plugin URI: http://www.beapi.fr
- * Description: This plugin allow to build development environnement without copy data from uploads folder. Manage an failback with PHP and production assets.
+ * Description: This plugin allow to build development environment without copy data from uploads folder. Manage an failback with PHP and production assets.
  * Author: BeAPI
  * Author URI: http://www.beapi.fr
  * Domain Path: languages
@@ -27,7 +27,7 @@
  *
  * --------------
  * 
- * 		Installation usage for WPms subfolder
+ * 		Installation usage for WP multisite
  * 
  * 		You need to add the following rule before this line "RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]"
  * 			RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-content/uploads.*) $1 [L]
@@ -42,45 +42,55 @@ if ( ! defined( 'PROD_UPLOADS_URL' ) ) {
 	define( 'PROD_UPLOADS_URL', 'http://myproddomain' );
 }
 
-if ( false !== strpos( $_SERVER['REQUEST_URI'], UPLOADS_STRUCTURE_NAME ) ) {
-	// Get extension
-	$extension = pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION );
+class Prod_Images {
+	function __construct() {
+		$this->replace_url();
+	}
 
-	// Send content type header
-	header( 'Content-Type: ' . get_mime_type_from_file_extension( $extension ) );
-
-	// Get remote HTML file
-	$response = wp_remote_get( untrailingslashit( PROD_UPLOADS_URL ) . $_SERVER['REQUEST_URI'] );
-
-	// Get response code
-	$response_code = wp_remote_retrieve_response_code( $response );
-
-	// Check for error and the response code
-	if ( ! is_wp_error( $response ) && 200 == $response_code ) {
-		// Parse remote HTML file
-		$data = wp_remote_retrieve_body( $response );
-
-		// Check for error
-		if ( ! is_wp_error( $data ) ) {
-			status_header( 200 );
-			echo $data;
-			exit();
+	public static function replace_url() {
+		if ( false === strpos( $_SERVER['REQUEST_URI'], UPLOADS_STRUCTURE_NAME ) ) {
+			return false;
 		}
+		// Get extension
+		$extension = pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION );
+
+		// Send content type header
+		header( 'Content-Type: ' . self::get_mime_type_from_file_extension( $extension ) );
+
+		// Get remote HTML file
+		$response = wp_remote_get( untrailingslashit( PROD_UPLOADS_URL ) . $_SERVER['REQUEST_URI'] );
+
+		// Get response code
+		$response_code = wp_remote_retrieve_response_code( $response );
+
+		// Check for error and the response code
+		if ( ! is_wp_error( $response ) && 200 == $response_code ) {
+			// Parse remote HTML file
+			$data = wp_remote_retrieve_body( $response );
+			// Check for error
+			if ( ! is_wp_error( $data ) ) {
+				status_header( 200 );
+				echo $data;
+				exit();
+			}
+		}
+
+		header( 'Content-Length: 0' );
+		exit();
 	}
 
-	header( 'Content-Length: 0' );
-	exit();
-}
+	public static function get_mime_type_from_file_extension( $extension ) {
+		global $phpmailer;
 
-function get_mime_type_from_file_extension( $extension ) {
-	global $phpmailer;
+		// (Re)create it, if it's gone missing
+		if ( ! ( $phpmailer instanceof PHPMailer ) ) {
+			require_once ABSPATH . WPINC . '/class-phpmailer.php';
+			require_once ABSPATH . WPINC . '/class-smtp.php';
+			$phpmailer = new PHPMailer( true );
+		}
 
-	// (Re)create it, if it's gone missing
-	if ( ! ( $phpmailer instanceof PHPMailer ) ) {
-		require_once ABSPATH . WPINC . '/class-phpmailer.php';
-		require_once ABSPATH . WPINC . '/class-smtp.php';
-		$phpmailer = new PHPMailer( true );
+		return $phpmailer->_mime_types( $extension );
 	}
-
-	return $phpmailer->_mime_types( $extension );
 }
+
+new Prod_Images;
