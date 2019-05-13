b3e62d24925947d8b88acbdaada3873b1a42e66b
Amaury Balmer <amaury@beapi.fr>
2019-05-13 20:29:00 +0200

    prepare 1.1.9 releaseé

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Prod Images #
 
+[![CodeFactor](https://www.codefactor.io/repository/github/beapi/prod-images/badge)](https://www.codefactor.io/repository/github/beapi/prod-images)
+
 ## Description ##
 
 This plugin allow to build development environment without copy data from uploads folder. Manage an failback with PHP and production assets.
 
-## Important to know ##
+## Installation ##
 
 Define constants :
 ```
@@ -12,7 +14,8 @@ define( 'UPLOADS_STRUCTURE_NAME', 'wp-content/uploads, wp-content/blogs.dir' );
 define( 'PROD_UPLOADS_URL', 'http://myproddomain' );
 ```
 
-If installation WP Multisite :
+#### If WordPress Multisite + Apache HTTPD
+
 You need to add the following rule before this line **wp-(content|admin|includes).*)**
 ```
 RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-content/uploads.*) $1 [L]
@@ -23,6 +26,38 @@ If your Multisite installation is an old verison with the blog.dir folder, you h
 ```
 RewriteRule ^([_0-9a-zA-Z-]+/)?files/(.+) $2 [L]
 ```
+
+#### If WordPress + Nginx
+
+It is likely that the NGINX configuration looks like this:
+```
+location ~* ^.+\.(ogg|ogv|svg|svgz|mp4|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|$
+    # access_log off;                                                                                          
+    # log_not_found off;
+                                                                                    
+    expires max;                                                                                                                                                              
+} 
+```
+
+With this configuration, there is no possible failback with WordPress, you must add the following statement in the condition:
+
+```                                                                                 
+try_files $uri $uri/ /index.php?$args;                                                                    
+```
+
+Full example :
+```
+location ~* ^.+\.(ogg|ogv|svg|svgz|mp4|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|$
+    # access_log off;                                                                                          
+    # log_not_found off;
+                                                                                    
+    expires max;                                                                                             
+    try_files $uri $uri/ /index.php?$args;                                                                   
+} 
+```
+
+
+## Extra config ##
 
 And in the wp-config.php
 ```
@@ -35,6 +70,12 @@ define( 'PROD_SSL_VERIFY', false );// default is true
 ```
 
 ## Changelog ##
+
+
+### 0.1.9
+* 13 May 2019
+* Refactoring mimes types check (add SVG support)
+* Improve readme (nginx part)
 
 ### 0.1.8
 * 18 December 2018
--- prod-images.php
+++ prod-images.php
@@ -1,16 +1,16 @@
 <?php
 /*
  * Plugin Name: BEA - Prod images
- * Version: 0.1.8
- * Plugin URI: http://www.beapi.fr
+ * Version: 0.1.9
+ * Plugin URI: https://beapi.fr
  * Description: This plugin allow to build development environment without copy data from uploads folder. Manage an failback with PHP and production assets.
- * Author: BeAPI
- * Author URI: http://www.beapi.fr
+ * Author: Be API
+ * Author URI: https://beapi.fr
  * Domain Path: languages
  * Network: false
  *
  * --------------
- * Copyright 2018 - BeAPI Team (human@beapi.fr)
+ * Copyright 2018-2019 - Be API Team (human@beapi.fr)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,16 +24,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
- *
- * --------------
- *
- * 		Installation usage for WP multisite
- *
- * 		You need to add the following rule before this line "RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]"
- * 			RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-content/uploads.*) $1 [L]
- *
- *      Warning :  If define( 'WP_HTTP_BLOCK_EXTERNAL', true ); so, define( 'WP_ACCESSIBLE_HOSTS', 'url_prod' );
- *
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -58,9 +48,11 @@ if ( ! defined( 'PROD_SSL_VERIFY' ) ) {
 class Prod_Images {
 
 	/**
-	 * @return bool
+	 * Check for image URL error, and load image as a proxy
+	 *
+	 * @return void
 	 */
-	public function replace_url() {
+	public function __construct() {
 		ob_start();
 
 		$_SERVER['_REQUEST_URI'] = untrailingslashit( $_SERVER['REQUEST_URI'] );
@@ -75,7 +67,7 @@ class Prod_Images {
 		}
 
 		if ( false === $flag ) {
-			return false;
+			return;
 		}
 
 		// Fix conflict with WProcket
@@ -85,7 +77,7 @@ class Prod_Images {
 		$extension = pathinfo( $_SERVER['_REQUEST_URI'], PATHINFO_EXTENSION );
 
 		// Send content type header
-		header( 'Content-Type: ' . $this->get_mime_type_from_file_extension( $extension ) );
+		header( 'Content-Type: ' . $this->get_mime_types( $extension ) );
 
 		// Send content proxy name
 		header( 'WP-Proxy: prod-images' );
@@ -125,32 +117,133 @@ class Prod_Images {
 			}
 		}
 
-		//TODO Improve cache
 		header( 'Pragma: public' );
 		header( 'Cache-Control: max-age=86400' );
-		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', time() + 86400 ) );
+		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', time() + 86400 ) ); // 24 hours
 		//header( 'Content-Length: 0' );
+
 		exit();
 	}
 
-	/**
-	 * @param $extension
-	 *
-	 * @return mixed
-	 */
-	public function get_mime_type_from_file_extension( $extension ) {
-		global $my_phpmailer;
 
-		// (Re)create it, if it's gone missing
-		if ( ! ( $my_phpmailer instanceof PHPMailer ) ) {
-			require_once ABSPATH . WPINC . '/class-phpmailer.php';
-			require_once ABSPATH . WPINC . '/class-smtp.php';
-			$my_phpmailer = new PHPMailer( true );
+	/**
+	 * Get the MIME type for a file extension.
+	 * Copy from --- wp-includes/class-phpmailer.php
+	 *
+	 * @param string $ext File extension
+	 *
+	 * @access public
+	 * @return string MIME type of file.
+	 */
+	public function get_mime_types( $ext = '' ) {
+		$mimes = array(
+			'xl'    => 'application/excel',
+			'js'    => 'application/javascript',
+			'hqx'   => 'application/mac-binhex40',
+			'cpt'   => 'application/mac-compactpro',
+			'bin'   => 'application/macbinary',
+			'doc'   => 'application/msword',
+			'word'  => 'application/msword',
+			'xlsx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+			'xltx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+			'potx'  => 'application/vnd.openxmlformats-officedocument.presentationml.template',
+			'ppsx'  => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+			'pptx'  => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+			'sldx'  => 'application/vnd.openxmlformats-officedocument.presentationml.slide',
+			'docx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+			'dotx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+			'xlam'  => 'application/vnd.ms-excel.addin.macroEnabled.12',
+			'xlsb'  => 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+			'class' => 'application/octet-stream',
+			'dll'   => 'application/octet-stream',
+			'dms'   => 'application/octet-stream',
+			'exe'   => 'application/octet-stream',
+			'lha'   => 'application/octet-stream',
+			'lzh'   => 'application/octet-stream',
+			'psd'   => 'application/octet-stream',
+			'sea'   => 'application/octet-stream',
+			'so'    => 'application/octet-stream',
+			'oda'   => 'application/oda',
+			'pdf'   => 'application/pdf',
+			'ai'    => 'application/postscript',
+			'eps'   => 'application/postscript',
+			'ps'    => 'application/postscript',
+			'smi'   => 'application/smil',
+			'smil'  => 'application/smil',
+			'mif'   => 'application/vnd.mif',
+			'xls'   => 'application/vnd.ms-excel',
+			'ppt'   => 'application/vnd.ms-powerpoint',
+			'wbxml' => 'application/vnd.wap.wbxml',
+			'wmlc'  => 'application/vnd.wap.wmlc',
+			'dcr'   => 'application/x-director',
+			'dir'   => 'application/x-director',
+			'dxr'   => 'application/x-director',
+			'dvi'   => 'application/x-dvi',
+			'gtar'  => 'application/x-gtar',
+			'php3'  => 'application/x-httpd-php',
+			'php4'  => 'application/x-httpd-php',
+			'php'   => 'application/x-httpd-php',
+			'phtml' => 'application/x-httpd-php',
+			'phps'  => 'application/x-httpd-php-source',
+			'swf'   => 'application/x-shockwave-flash',
+			'sit'   => 'application/x-stuffit',
+			'tar'   => 'application/x-tar',
+			'tgz'   => 'application/x-tar',
+			'xht'   => 'application/xhtml+xml',
+			'xhtml' => 'application/xhtml+xml',
+			'zip'   => 'application/zip',
+			'mid'   => 'audio/midi',
+			'midi'  => 'audio/midi',
+			'mp2'   => 'audio/mpeg',
+			'mp3'   => 'audio/mpeg',
+			'mpga'  => 'audio/mpeg',
+			'aif'   => 'audio/x-aiff',
+			'aifc'  => 'audio/x-aiff',
+			'aiff'  => 'audio/x-aiff',
+			'ram'   => 'audio/x-pn-realaudio',
+			'rm'    => 'audio/x-pn-realaudio',
+			'rpm'   => 'audio/x-pn-realaudio-plugin',
+			'ra'    => 'audio/x-realaudio',
+			'wav'   => 'audio/x-wav',
+			'bmp'   => 'image/bmp',
+			'gif'   => 'image/gif',
+			'jpeg'  => 'image/jpeg',
+			'jpe'   => 'image/jpeg',
+			'jpg'   => 'image/jpeg',
+			'png'   => 'image/png',
+			'tiff'  => 'image/tiff',
+			'tif'   => 'image/tiff',
+			'eml'   => 'message/rfc822',
+			'css'   => 'text/css',
+			'html'  => 'text/html',
+			'htm'   => 'text/html',
+			'shtml' => 'text/html',
+			'log'   => 'text/plain',
+			'text'  => 'text/plain',
+			'txt'   => 'text/plain',
+			'rtx'   => 'text/richtext',
+			'rtf'   => 'text/rtf',
+			'vcf'   => 'text/vcard',
+			'vcard' => 'text/vcard',
+			'xml'   => 'text/xml',
+			'xsl'   => 'text/xml',
+			'mpeg'  => 'video/mpeg',
+			'mpe'   => 'video/mpeg',
+			'mpg'   => 'video/mpeg',
+			'mov'   => 'video/quicktime',
+			'qt'    => 'video/quicktime',
+			'rv'    => 'video/vnd.rn-realvideo',
+			'avi'   => 'video/x-msvideo',
+			'movie' => 'video/x-sgi-movie',
+			'svg'   => 'image/svg+xml', // Added by BeAPI
+		);
+
+		if ( array_key_exists( strtolower( $ext ), $mimes ) ) {
+			return $mimes[ strtolower( $ext ) ];
 		}
 
-		return $my_phpmailer->_mime_types( $extension );
+		return 'application/octet-stream';
 	}
 }
 
-$prod_images = new Prod_Images();
-$prod_images->replace_url();
+new Prod_Images();
